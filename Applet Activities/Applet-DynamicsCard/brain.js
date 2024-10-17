fe93c5596e12545a8b2960cd42adb436ac777cee
japowww <salesjaspherjules@gmail.com>
2024-10-17 13:17:06 +0800

    Adding the json data's

--- Applet Activities/Applet-DynamicsCard/brain.js
+++ Applet Activities/Applet-DynamicsCard/brain.js
@@ -1,52 +1,53 @@
-class Restaurantlist{
-    constructor(dataUrl){
+class Restaurantlist {
+    constructor(dataUrl) {
         this.dataUrl = dataUrl;
         this.restaurant = [];
-        this.init()
+        this.init();
     }
 
-     async init(){
+    async init() {
         await this.fetchData();
         this.renderRestaurantlist(this.restaurant);
         this.bindSearchEvent();
-     }
+    }
 
-     async fetchData(){
-        try{
-            const response = await fetch (this.dataUrl);
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
             this.restaurant = await response.json();
+        } catch (error) {
+            console.error(`Error ka dai:`, error);
         }
-        catch(Error){
-            console.error(` Error ka dai`, error);
+    }
+
+    renderRestaurantlist(resto, container) {
+        if (!container) {
+            container = document.getElementById(`restaurantlist`);
         }
-     }
+        container.innerHTML = resto.map(restaurant => `
+            <button class="btn btn-primary" style="margin-top: 15px; width: 25rem;">
+                ${restaurant.restaurant_name} | ${restaurant.restaurant_location}
+            </button><br>
+        `).join('');
+    }
 
-     renderRestaurantlist(restaurant){
-         const restaurantlistcontainer = document.getElementById(`restaurantlist`);
-         restaurantlistcontainer.innerHTML =  restaurant.map(restaurant => `<button class=" btn btn-primary style="margin-top: 15px" width: 25rem;
-             ${restaurant.restaurant_name} | ${restaurant.restaurant_location} </button> <br>`).join('');    
-     }
-
-     bindSearchEvent(){
+    bindSearchEvent() {
         const restaurantSearchBar = document.getElementById(`restaurantSearchBar`);
         const restaurantSearchListContainer = document.getElementById(`restaurantSearchList`);
 
-        restaurantSearchBar.addEventListener(`input`, ()=>  {this.filterrestaurant(restaurantSearchBar.value, restaurantSearchListContainer);});
-        
-        this.renderRestaurantlist(this.restaurant, restaurantSearchListContainer);
-     }
+        restaurantSearchBar.addEventListener(`input`, () => {
+            this.filterrestaurant(restaurantSearchBar.value, restaurantSearchListContainer);
+        });
+    }
 
-     filterrestaurant(query, restaurantSearchListContainer ){
-        const filteredRestaurants = this.restaurant.filter( restaurant => {
-            const RestaurantName = `${restaurant.restaurant_name} ${restaurant.restaurant_location}`;
-            return RestaurantName.toLowerCase().includes(query, restaurantSearchListContainer());
-
+    filterrestaurant(query, restaurantSearchListContainer) {
+        const filteredRestaurants = this.restaurant.filter(restaurant => {
+            const restaurantName = `${restaurant.restaurant_name} ${restaurant.restaurant_location}`;
+            return restaurantName.toLowerCase().includes(query.toLowerCase());
         });
 
-        restaurantSearchListContainer.innerHTML =  ``;
-
         this.renderRestaurantlist(filteredRestaurants, restaurantSearchListContainer);
-
-     }
+    }
 }
-const resList = new Restaurantlist
+
+const resList = new Restaurantlist(`jajs.json`);
