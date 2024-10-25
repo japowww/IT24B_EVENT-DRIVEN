917dee91293c18ddd9809b2e84e70fa0f6add42f
japowww <salesjaspherjules@gmail.com>
2024-10-25 22:35:50 +0800

    Starting the TodoList script

---
+++ Applet Activities/Applet-ToDoList/TDlist.js
@@ -0,0 +1,8 @@
+class todolist{
+    constructor(){
+        this.EditingIndex = -1;
+        this.addButton = document.getElementById(`addButton`);
+        this.Input = document.getElementById(`Input`);
+        
+    }
+}
