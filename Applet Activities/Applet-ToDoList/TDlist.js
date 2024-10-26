607c8a1a8f2862653f6daf02195fd7d35f6a5414
japowww <salesjaspherjules@gmail.com>
2024-10-26 23:17:12 +0800

    molding addorUpdateTask

--- Applet Activities/Applet-ToDoList/TDlist.js
+++ Applet Activities/Applet-ToDoList/TDlist.js
@@ -8,4 +8,12 @@ class todolist{
         this.addButton = addEventListener(`click`, () => this.addOrUpdateTask());
         this.todolist.addEventListener(`click`, (e) => this.handlebuttnclic(e));  
     }
+    addOrUpdateTask() {
+        const taskText = this.todoInput.value.trim();
+        if (taskText) {
+            this.editingIndex === -1 ? this.addTask(taskText) : this.updateTask(taskText);
+            this.todoInput.value = '';
+            this.resetEditing();
+        }
+    }
 }
