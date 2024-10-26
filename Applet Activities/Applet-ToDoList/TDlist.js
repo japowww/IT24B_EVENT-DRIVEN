162ec950449c0b1df1b2f58576308890b8d7e4ce
japowww <salesjaspherjules@gmail.com>
2024-10-26 23:32:49 +0800

    creating the  addtask function

--- Applet Activities/Applet-ToDoList/TDlist.js
+++ Applet Activities/Applet-ToDoList/TDlist.js
@@ -16,4 +16,18 @@ class todolist{
             this.resetEditing();
         }
     }
+    
+    addTask(taskText){
+const   Listitem = document.createElement(`li`);
+Listitem.innerHTML = `
+            <span class="task-text">${taskText}</span>
+            <span class="timestamp" style="display: block; margin-top: 0.5rem; color: gray;">Date Added: ${new Date().toLocaleString()}</span>
+            <div style="margin-top: 0.5rem;">
+                <button class="btn btn-success btn-sm doneButton">Done</button>
+                <button class="btn btn-warning btn-sm editButton">Edit</button>
+                <button class="btn btn-danger btn-sm removeButton">Remove</button>
+            </div>
+        `;
+        this.todoList.appendChild(listItem);
+    }
 }
