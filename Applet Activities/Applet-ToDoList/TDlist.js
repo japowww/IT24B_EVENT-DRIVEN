5a26cf933d05d9667710ee57353c61b0e5f15506
japowww <salesjaspherjules@gmail.com>
2024-10-26 22:43:08 +0800

    Class todo list finished

--- Applet Activities/Applet-ToDoList/TDlist.js
+++ Applet Activities/Applet-ToDoList/TDlist.js
@@ -3,6 +3,9 @@ class todolist{
         this.EditingIndex = -1;
         this.addButton = document.getElementById(`addButton`);
         this.Input = document.getElementById(`Input`);
-        
+        this.todolist = document.getElementById(`todolist`);
+
+        this.addButton = addEventListener(`click`, () => this.addOrUpdateTask());
+        this.todolist.addEventListener(`click`, (e) => this.handlebuttnclic(e));  
     }
 }
