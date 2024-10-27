b2704021a30c7d47751cb57cb3994e3ce4e36a09
japowww <salesjaspherjules@gmail.com>
2024-10-27 23:15:39 +0800

    Starting the js class and added the object

---
+++ Applet Activities/Applet-OpenWeather/weathe.js
@@ -0,0 +1,8 @@
+class WeatherApp{
+    constructor() {
+        this.apiKey = document.getElementById('apiKeyInput').value;
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+        this.getLocationBtn = document.getElementById('getLocationBtn');
+}
+}
