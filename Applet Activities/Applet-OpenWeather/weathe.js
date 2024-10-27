4b7c3d652f868b115a621bc0c12bed5f3a9003ec
japowww <salesjaspherjules@gmail.com>
2024-10-27 23:16:37 +0800

    inserting weather elements and event listeners

--- Applet Activities/Applet-OpenWeather/weathe.js
+++ Applet Activities/Applet-OpenWeather/weathe.js
@@ -4,5 +4,16 @@ class WeatherApp{
         this.cityInput = document.getElementById('cityInput');
         this.getWeatherBtn = document.getElementById('getWeatherBtn');
         this.getLocationBtn = document.getElementById('getLocationBtn');
+
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+        this.weatherIcon = document.getElementById('weatherIcon');
+
+        this.getWeatherBtn.addEventListener('click', () => this.getWeather());
+        this.getLocationBtn.addEventListener('click', () => this.getWeatherByLocation());
 }
 }
